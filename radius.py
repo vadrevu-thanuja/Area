5fc3332bbc936ff7524bcffb74684c08aa1401f6
vadrevu-thanuja <68389998+vadrevu-thanuja@users.noreply.github.com>
2020-07-18 13:31:14 +0530

    updated radius.p

--- radius.py
+++ radius.py
@@ -1,4 +1,4 @@
 PI=3.14
 r=float(input('input the radius of the circle :')
 area=PI*r*r
-print("The aea of the circle with  radius "+str(r)+"is:"+area)
+print("The aea of the circle with radius is %2f"%area)
